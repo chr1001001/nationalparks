b85315e41e06b8dd67f66f62f21f21c4dad9dfa5
chr1001001 <114487517+chr1001001@users.noreply.github.com>
2022-09-27 14:49:46 +0200

    Update script.sh

--- .github/script.sh
+++ .github/script.sh
@@ -4,6 +4,10 @@ app_name=$1
 image=$2
 namespace=$3
 
+echo $1
+echo $2
+echo $3
+
 deploy=`oc get deployment $app_name -n $namespace`
 if [[ "$?" -eq 0 ]]; then
     oc set image deployment/$app_name $app_name=$image -n $namespace
